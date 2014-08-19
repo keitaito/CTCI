5856e5f0cce35ae4e75dcc6e7334117929b60ec1
Keita Ito <gi1okt@gmail.com>
2014-08-18 22:18:05 -0700

    Initial Commit

---
+++ CTCITests/CTCITests.m
@@ -0,0 +1,34 @@
+//
+//  CTCITests.m
+//  CTCITests
+//
+//  Created by Keita on 8/18/14.
+//  Copyright (c) 2014 Keita Ito. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CTCITests : XCTestCase
+
+@end
+
+@implementation CTCITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
